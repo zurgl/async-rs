a98c1de1c7704fc3a74b649cca56522d08812f34
zurgl <elayar.yacine@gamil.com>
2023-04-18 15:12:23 +0200

    before 5

--- async04/src/tj.rs
+++ async04/src/tj.rs
@@ -4,28 +4,35 @@ use std::{
     task::{Context, Poll},
 };
 
-struct TryJoin<A, B, AR, BR, E>
-where
-    A: Future<Output = Result<AR, E>>,
-    B: Future<Output = Result<BR, E>>,
-{
-    a: A,
-    b: B,
-    a_res: Option<AR>,
-    b_res: Option<BR>,
-}
-
 pub fn try_join<A, B, AR, BR, E>(a: A, b: B) -> impl Future<Output = Result<(AR, BR), E>>
 where
     A: Future<Output = Result<AR, E>>,
     B: Future<Output = Result<BR, E>>,
 {
-    TryJoin {
-        a,
-        b,
-        a_res: None,
-        b_res: None,
+    TryJoin::Polling {
+        a: State::Future(a),
+        b: State::Future(b),
     }
+}
+
+enum State<F, T, E>
+where
+    F: Future<Output = Result<T, E>>,
+{
+    Future(F),
+    Ok(T),
+}
+
+enum TryJoin<A, B, AR, BR, E>
+where
+    A: Future<Output = Result<AR, E>>,
+    B: Future<Output = Result<BR, E>>,
+{
+    Polling {
+        a: State<A, AR, E>,
+        b: State<B, BR, E>,
+    },
+    Done,
 }
 
 impl<A, B, AR, BR, E> Future for TryJoin<A, B, AR, BR, E>
@@ -37,37 +44,32 @@ where
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let this = unsafe { self.get_unchecked_mut() };
-        let (a, b) = unsafe {
-            (
-                Pin::new_unchecked(&mut this.a),
-                Pin::new_unchecked(&mut this.b),
-            )
+        let (a, b) = match this {
+            Self::Polling { a, b } => (a, b),
+            Self::Done => panic!("TryJoin future polled after completion"),
         };
 
-        if this.a_res.is_none() {
-            if let Poll::Ready(res) = a.poll(cx) {
-                match res {
-                    Ok(x) => this.a_res = Some(x),
-                    Err(e) => return Err(e).into(),
-                }
+        if let State::Future(fut) = a {
+            if let Poll::Ready(res) = unsafe { Pin::new_unchecked(fut) }.poll(cx) {
+                *a = State::Ok(res?);
             }
         }
 
-        if this.b_res.is_none() {
-            if let Poll::Ready(res) = b.poll(cx) {
-                match res {
-                    Ok(x) => this.b_res = Some(x),
-                    Err(e) => return Err(e).into(),
-                }
+        if let State::Future(fut) = b {
+            if let Poll::Ready(res) = unsafe { Pin::new_unchecked(fut) }.poll(cx) {
+                *b = State::Ok(res?);
             }
         }
 
-        if let (Some(_), Some(_)) = (&this.a_res, &this.b_res) {
-            let a = this.a_res.take().unwrap();
-            let b = this.b_res.take().unwrap();
-            Ok((a, b)).into()
-        } else {
-            Poll::Pending
+        match (a, b) {
+            (State::Ok(_), State::Ok(_)) => match std::mem::replace(this, Self::Done) {
+                Self::Polling {
+                    a: State::Ok(a),
+                    b: State::Ok(b),
+                } => Ok((a, b)).into(),
+                _ => unreachable!(),
+            },
+            _ => Poll::Pending,
         }
     }
 }
